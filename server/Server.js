ecc7e50f1569dfaa31440356d39a91827d820328
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-09-24 19:09:33 +0530

    still trying

--- server/Server.js
+++ server/Server.js
@@ -6,15 +6,14 @@ import { clerkWebhooks } from './controllers/webhooks.js';
 
 
 const PORT = process.env.PORT || 5000;
+const app = express();
 await connectDB();
 
-const app = express();
 app.use(cors());
-app.use(express.json());
 
 
 
 app.get('/', (req, res) => res.send('API is running...'));
-app.post('/clerk', clerkWebhooks);
+app.post('/clerk',express.json(), clerkWebhooks);
 
 app.listen(PORT, () => console.log(`Server start on ${PORT}`));
