0f88672c279b7aafffa3bd85cdaec6fd19feb923
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-09-24 18:50:31 +0530

    try to solve

--- server/Server.js
+++ server/Server.js
@@ -1,19 +1,20 @@
-// api/index.js
 import express from 'express';
-import serverless from 'serverless-http';
 import cors from 'cors';
 import 'dotenv/config';
-import connectDB from '../config/mongoDB.js';
-import { clerkWebhooks } from '../controllers/webhooks.js';
+import connectDB from './config/mongoDB.js';
+import { clerkWebhooks } from './controllers/webhooks.js';
+
+
+const PORT = process.env.PORT || 5000;
+await connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-await connectDB();
 
-app.get('/favicon.ico', (req, res) => res.status(204).end());
+
 app.get('/', (req, res) => res.send('API is running...'));
 app.post('/clerk', clerkWebhooks);
 
-export const handler = serverless(app);
+app.listen(PORT, () => console.log(`Server start on ${PORT}`));
