0f594fc4ca7a7925cf4ab69ce819d3f1d5814407
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-09-25 11:21:06 +0530

    Feat: setup clerk and getting user

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from 'humanize-duration'
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 const AppContext = createContext();
 
@@ -10,6 +11,11 @@ export const AppContextProvider = (props) => {
 
     const currency = import.meta.env.VITE_CURRENCY; 
     const navigate = useNavigate();
+
+
+    const {getToken} = useAuth()
+    const {user} = useUser()
+
     const [allCourses, setAllCourses] = useState([]);
     const [isEducator, setIsEducator] = useState(true);
     const [enrolledCourses, setEnrolledCourses] = useState([]);
@@ -70,6 +76,17 @@ export const AppContextProvider = (props) => {
         fetchAllCourses()
         fetchUserEnrolledCourses()
     }, []);
+
+    const logToken = async () => {
+        console.log(await getToken());
+        
+    }
+
+    useEffect(()=> {
+        if (user) {
+            logToken()
+        }
+    }, [user])
 
     const value = {
         currency,
--- server/Server.js
+++ server/Server.js
@@ -3,17 +3,24 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongoDB.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinary from './config/cloudinary.js';
 
+
+const app = express();
+
+// Middlewares 
+app.use(cors());
+app.use(clerkMiddleware());
+
+connectDB();
+await connectCloudinary()
 
 const PORT = process.env.PORT || 5000;
-const app = express();
-await connectDB();
-
-app.use(cors());
-
-
 
 app.get('/', (req, res) => res.send('API is running...'));
-app.post('/clerk',express.json(), clerkWebhooks);
+app.post('/clerk', express.json(), clerkWebhooks);
+app.use('/api/educator', express.json(), educatorRouter)
 
 app.listen(PORT, () => console.log(`Server start on ${PORT}`));
